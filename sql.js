c2ed3676949a999591be8c55ccceac9d75e1e760
Jacques Joky <jacques.joky@gmail.com>
2020-06-01 19:36:19 -0400

    mostSummerWins

--- sql.js
+++ sql.js
@@ -40,7 +40,7 @@ Returns a SQL query string that will find the number of gold medals for the give
 const goldMedalNumber = country => {
     return `SELECT COUNT(*)
     FROM GoldMedal
-    WHERE country = '${country}'`;
+    WHERE country = '${country}';`;
 };
 
 /*
@@ -49,7 +49,14 @@ won the most summer medals, along with the number of medals aliased to 'count'.
 */
 
 const mostSummerWins = country => {
-  return;
+  return `SELECT year,
+  COUNT(*) AS count
+  FROM GoldMedal
+  WHERE country = '${country}'
+  AND season = 'Summer'
+  GROUP BY year
+  ORDER BY count DESC
+  LIMIT 1;`;
 };
 
 /*
